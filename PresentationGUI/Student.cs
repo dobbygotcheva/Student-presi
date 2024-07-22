1f3bcdf0e4f95aa5172b2ea9d54e01903e4a1b62
dobbygotcheva <146433766+dobbygotcheva@users.noreply.github.com>
2024-07-22 17:54:44 +0300

    Add files via upload

---
+++ PresentationGUI/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationGUI
+{
+    public abstract class Student : Person, Itraveler
+    {
+        private string major;
+        private string studentId;
+    
+
+        public Student()
+            : base()
+        {
+        }
+
+        // Constructor which sends 3 arguments to base class
+        public Student(string id, string fname, 
+             string lname, string maj, string sId)
+            : base(id, lname, fname)
+        {
+            major = maj;
+            studentId = sId;
+        }
+
+        public Student(string id, string maj)
+            : base(id)
+        {
+            major = maj;
+            //studentId = sId;
+        }
+
+        // Read only Property for studentID
+        public string StudentId
+        {
+            get
+            {
+                return studentId;
+            }
+        }
+
+        
+
+        // Property for major data field
+        public string Major
+        {
+            get
+            {
+                return major;
+            }
+            set
+            {
+                major = value;
+            }
+        }
+
+        // Overrides GetSleepAmt( ) 
+        // method of the Person class
+        public override int GetSleepAmt()
+        {
+            return 6;
+        }
+
+        // Using the base keyword, calls 
+        // the overridden GetSleepAmt( )
+        // method of the Person class
+        public int CallOverriddenGetSleepAmt()
+        {
+            return base.GetSleepAmt();
+        }
+
+        public override string GetExerciseHabits()
+        {
+            return "Exercises daily";
+        }
+
+        public string GetDestination()
+        {
+            return "Home";
+        }
+
+        public string GetStartLocation()
+        {
+            return "School";
+
+        }
+
+        public double DetermineMiles()
+        {
+            return 75.0;
+        }
+    }
+}
